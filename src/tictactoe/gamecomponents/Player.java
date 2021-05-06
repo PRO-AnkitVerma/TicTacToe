fe6184c1aef32c9f7e5b0416366e9aed605f5170
PRO-AnkitVerma <ankitverma209171@gmail.com>
2021-05-06 12:30:41 +0530

    Added Game Components

---
+++ src/tictactoe/gamecomponents/Player.java
@@ -0,0 +1,27 @@
+package tictactoe.gamecomponents;
+
+import tictactoe.utils.PlayerType;
+import tictactoe.utils.Position;
+import tictactoe.utils.Symbol;
+
+import java.util.ArrayList;
+
+public class Player {
+    private final PlayerType playerType;
+    private Symbol symbol;
+
+    public Player() {
+        playerType = PlayerType.HUMAN;
+    }
+
+    public Player(PlayerType playerType) {
+        this.playerType = playerType;
+    }
+
+    public Position getMove(Board board) {
+        ArrayList<ArrayList<Square>> grid = board.getGrid();
+        //TODO: implement backtracking algorithm for calculating optimum move
+        return new Position(0, 0);
+    }
+
+}
